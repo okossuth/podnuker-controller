9ee93bafa7d88b9ca67dffc1a0d1c372d80a5c10
Oskar Kossuth <oskar@incsub.com>
2023-08-21 15:53:58 -0300

    Added improvements

--- podnuker-controller.py
+++ podnuker-controller.py
@@ -3,36 +3,40 @@ import logging
 import kubernetes
 from kubernetes.client.rest import ApiException
 
-
+# Load in-cluster configuration for accessing the Kubernetes API from within a pod running inside the cluster
 kubernetes.config.load_incluster_config()
 
-logging.basicConfig(level=logging.INFO)
+# Set the default configuration for logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
+# Create api_instance object used to interact with the Kubernetes API for core resources
 api_instance = kubernetes.client.CoreV1Api()
 
+#Define K8s namespaces to monitor for evicted pods
 namespaces=["default", "keda"]
 
+# Main logic
 while True: 
     for namespace in namespaces:
         try:
-            ret = api_instance.list_namespaced_pod(namespace, watch=False)
+            # Get list of pods in namespace
+            pods = api_instance.list_namespaced_pod(namespace, watch=False)
         except ApiException as e:
-            #print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
             logging.info(f"Exception when calling CoreV1Api->list_namespaced_pod: %s\n", e)
             break
 
-        for i in ret.items:
-            if i.status.reason == "Evicted":
-                #print (i.metadata.name, i.status.reason)
-                logging.info(f"Pod: %s - Status: %s", i.metadata.name, i.status.reason)
+        for pod in pods.items:
+            # If pod status is Evicted
+            if pod.status.reason == "Evicted":
+                logging.info(f"Pod: %s - Status: %s", pod.metadata.name, pod.status.reason)
                 try:
-                    #print("Deleting Evicted Pod %s" % i.metadata.name)
-                    logging.info(f"Deleting Evicted Pod %s", i.metadata.name)
-                    api_response = api_instance.delete_namespaced_pod(i.metadata.name, namespace)
-                    print(api_response)
+                    logging.info(f"Deleting Evicted Pod %s", pod.metadata.name)
+                    # Delete pod from namespace
+                    api_response = api_instance.delete_namespaced_pod(pod.metadata.name, namespace)
+                    print(api_response.status.message)
                 except ApiException as e:
-                    #print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
                     logging.info(f"Exception when calling CoreV1Api->delete_namespaced_pod: %s\n", e)
         logging.info(f"All pods are running fine in namespace %s", namespace)
-        
+       
+    # Sleep for 30 minutes   
     time.sleep(1800)
